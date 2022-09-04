bc4dc22c2840871459df097be6c95861d5e24cd7
Rodrigo Camelo <83035616+RodrigoCameloo@users.noreply.github.com>
2022-09-04 15:32:33 +0000

    Update Containerregistry.tf

--- Containerregistry.tf
+++ Containerregistry.tf
@@ -9,3 +9,20 @@ resource "azurerm_resource_group" "rg" {
   name = "rg-acr"
   location = "brasilsouth"
 }
+
+  resource "azure_container_registry" "acr" {
+    name              ="acregistrytf"
+    resource_group_name= azurerm_resource_group.rg.name
+    location          = azurerm_resource_group.rg.location
+    sku               ="basic"
+    admin_enabled     =true
+}
+
+output "admin-usuario" {
+    value = azurerm_container_registry.acr.admin_username
+}
+
+output "admin-senha" {
+    value = azurerm_container_registry.acr.admin_password
+  
+}
